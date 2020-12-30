a9b1ab0c38669a10f1b9b35172736fdb3b09eaa5
Stamatina Barakou <matbarakou@gmail.com>
2020-12-30 13:14:26 +0200

    added css locators

---
+++ Book_Scraping_app/locators/book_locators.py
@@ -0,0 +1,6 @@
+class BookLocators:
+    title='article.product_pod h3 a'
+    link='article.product_pod h3 a'
+    stars='article.product_pod p.star-rating'
+    price='article.product_pod div.product_price p'
+
